740d472887111a5662e4afce3742e7d5f112a997
Malnati <ricardomalnati@gmail.com>
2024-10-10 13:18:21 -0300

    Refactor code to configure and use GPT-Neo model for generating table comments

--- src/gen_table_comment_codebert.py
+++ src/gen_table_comment_codebert.py
@@ -1,25 +1,28 @@
 import json
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+from transformers import AutoModelForMaskedLM, AutoTokenizer, pipeline
 
 def configure_codebert_pipeline():
     model_name = "microsoft/codebert-base"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    codebert_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    return codebert_pipeline
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    codebert_pipeline = pipeline("fill-mask", model=model, tokenizer=tokenizer)
+    return codebert_pipeline, tokenizer
 
-def generate_comment_with_codebert(codebert_pipeline, column_comments):
-    input_text = ". ".join(column_comments)
-    result = codebert_pipeline(input_text, max_new_tokens=150, truncation=True)
-    return result[0]['generated_text']
+def generate_comment_with_codebert(codebert_pipeline, tokenizer, column_comments):
+    # Combina os comentários e insere o token de máscara apropriado
+    mask_token = tokenizer.mask_token
+    input_text = ". ".join(column_comments)[:500] + f" {mask_token}."
+    result = codebert_pipeline(input_text)
+    # Obtém o resultado com a maior pontuação
+    return result[0]['sequence']
 
-def process_table_comments(input_file, output_file, codebert_pipeline):
+def process_table_comments(input_file, output_file, codebert_pipeline, tokenizer):
     with open(input_file, 'r') as f:
         schema_info = json.load(f)
 
     for table in schema_info:
         column_comments = [col['column_comment'] for col in table['columns']]
-        table_comment = generate_comment_with_codebert(codebert_pipeline, column_comments)
+        table_comment = generate_comment_with_codebert(codebert_pipeline, tokenizer, column_comments)
         table['table_comment'] = table_comment
 
     with open(output_file, 'w') as f:
@@ -29,5 +32,5 @@ def process_table_comments(input_file, output_file, codebert_pipeline):
 if __name__ == "__main__":
     input_file = './output/filtered_schema_info.json'
     output_file = './output/schema_with_table_comments_codebert.json'
-    codebert_pipeline = configure_codebert_pipeline()
-    process_table_comments(input_file, output_file, codebert_pipeline)
+    codebert_pipeline, tokenizer = configure_codebert_pipeline()
+    process_table_comments(input_file, output_file, codebert_pipeline, tokenizer)
