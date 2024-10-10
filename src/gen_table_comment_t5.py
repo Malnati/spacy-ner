d592fac0746ca69bf5bd550121595659252d5292
Malnati <ricardomalnati@gmail.com>
2024-10-10 13:14:29 -0300

    Refactor code to remove commented out max_length parameter in gen_table_comment_codebert.py and gen_table_comment_gpt_neo.py

--- src/gen_table_comment_t5.py
+++ src/gen_table_comment_t5.py
@@ -1,33 +1,42 @@
 import json
-from transformers import T5Tokenizer, T5ForConditionalGeneration, pipeline
+from transformers import T5ForConditionalGeneration, T5Tokenizer, pipeline
 
-def configure_codet5_pipeline():
-    model_name = "Salesforce/codet5-base"
+def configure_t5_pipeline():
+    model_name = "t5-base"
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
-    codet5_pipeline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
-    return codet5_pipeline
+    t5_pipeline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+    return t5_pipeline
 
-def generate_comment_with_codet5(codet5_pipeline, column_comments):
+def generate_comment_with_t5(t5_pipeline, column_comments):
     input_text = ". ".join(column_comments)
-    result = codet5_pipeline(input_text, max_new_tokens=150, truncation=True)
+    # Usar max_new_tokens para limitar a quantidade de texto gerado
+    result = t5_pipeline(input_text, max_new_tokens=150, truncation=True)
     return result[0]['generated_text']
 
-def process_table_comments(input_file, output_file, codet5_pipeline):
+def process_table_comments(input_file, output_file, t5_pipeline):
+    # Ler o arquivo filtrado
     with open(input_file, 'r') as f:
         schema_info = json.load(f)
 
+    # Para cada tabela, gerar o comentário com base nos comentários das colunas
     for table in schema_info:
         column_comments = [col['column_comment'] for col in table['columns']]
-        table_comment = generate_comment_with_codet5(codet5_pipeline, column_comments)
+        table_comment = generate_comment_with_t5(t5_pipeline, column_comments)
         table['table_comment'] = table_comment
 
+    # Salvar o resultado no arquivo de saída
     with open(output_file, 'w') as f:
         json.dump(schema_info, f, indent=2, ensure_ascii=False)
     print(f"Comments generated for tables saved to {output_file}")
 
 if __name__ == "__main__":
+    # Caminhos dos arquivos de entrada e saída
     input_file = './output/filtered_schema_info.json'
-    output_file = './output/schema_with_table_comments_codet5.json'
-    codet5_pipeline = configure_codet5_pipeline()
-    process_table_comments(input_file, output_file, codet5_pipeline)
+    output_file = './output/schema_with_table_comments_t5.json'
+
+    # Configurar o pipeline do T5
+    t5_pipeline = configure_t5_pipeline()
+
+    # Processar os comentários das tabelas
+    process_table_comments(input_file, output_file, t5_pipeline)
